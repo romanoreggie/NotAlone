d4430550a3d6a6697d43b389e29eeb3c56661ce4
Reggie Romano <romanoreggie@gmail.com>
2018-06-04 23:31:34 -0500

    Working on contact form

--- not-alone/src/component/SoberLiving.js
+++ not-alone/src/component/SoberLiving.js
@@ -20,6 +20,7 @@ const SoberLivingPage = () =>
           <p>Lorem ipsum dolor sit amet, tristique quia, quis sem neque porttitor. In posuere nunc pharetra nam id, mollis porttitor, neque viverra rutrum ullamcorper elit elit nullam, posuere felis mattis tortor ac. Neque massa ac id dui eros, scelerisque arcu platea massa molestie, erat justo tortor scelerisque sit neque leo, felis pede ut commodo. Amet semper diam consequat, suscipit condimentum rhoncus tellus wisi purus.</p>
           <h3> Item #5 </h3>
           <p>Lorem ipsum dolor sit amet, tristique quia, quis sem neque porttitor. In posuere nunc pharetra nam id, mollis porttitor, neque viverra rutrum ullamcorper elit elit nullam, posuere felis mattis tortor ac. Neque massa ac id dui eros, scelerisque arcu platea massa molestie, erat justo tortor scelerisque sit neque leo, felis pede ut commodo. Amet semper diam consequat, suscipit condimentum rhoncus tellus wisi purus.</p>
+          <h2>If you would like to be featured on this site contact us using the link below.</h2>
           </Container>
           <Footer />
         </div>
